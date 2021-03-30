62dc8e993b789718f5d2f10085203df64cd8ad7e
dfldiego <dfl.diego92@gmail.com>
2021-03-29 21:26:37 -0300

    Rutas en modulo.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//components
+import { ListaHeroesComponent } from './components/lista-heroes/lista-heroes.component';
+import { DetalleHeroeComponent } from './components/detalle-heroe/detalle-heroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: ListaHeroesComponent },
+  { path: '/marvel', component: ListaHeroesComponent },
+  { path: '/dc', component: ListaHeroesComponent },
+  { path: '/detalle-heroe/:id', component: DetalleHeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
